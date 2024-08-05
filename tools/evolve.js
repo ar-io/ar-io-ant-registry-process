3e8db9fb48eb930fa628881ceda18f0aa9b5694a
atticusofsparta <atticusofsparta@protonmail.com>
2024-08-05 11:59:52 -0600

    fix(naming): remove name clash

--- tools/evolve.js
+++ tools/evolve.js
@@ -5,11 +5,11 @@ import process from 'process';
 const registryId = process.env.REGISTRY_ID;
 const wallet = JSON.parse(process.env.WALLET);
 
-const process = new AOProcess({ processId: registryId });
+const registry = new AOProcess({ processId: registryId });
 
 const signer = createAoSigner(new ArweaveSigner(wallet));
 
-const evolveResult = await process.send({
+const evolveResult = await registry.send({
   tags: [{ name: 'Action', value: 'Eval' }],
   data: BUNDLED_AOS_LUA,
   signer,
