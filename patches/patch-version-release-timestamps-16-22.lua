be5280de8b719a1358748867c251fa6c9c2e6769
atticusofsparta <atticusofsparta@protonmail.com>
2025-08-07 12:43:09 -0600

    chore(create patch version file for release timestamps

---
+++ patches/patch-version-release-timestamps-16-22.lua
@@ -0,0 +1,7 @@
+ANTVersions["16"].releaseTimestamp = 1739236569000
+ANTVersions["17"].releaseTimestamp = 1741898395000
+ANTVersions["18"].releaseTimestamp = 1742933149000
+ANTVersions["19"].releaseTimestamp = 1749669994000
+ANTVersions["20"].releaseTimestamp = 1749768713000
+ANTVersions["21"].releaseTimestamp = 1750194882000
+ANTVersions["22"].releaseTimestamp = 1753595063000
